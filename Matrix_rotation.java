233f510b699e13a5d9cfac007281cc94a3c244da
INDUSAKTHIVEL <indusakthi2003@gmail.com>
2023-07-18 11:30:10 +0530

    rotatematrix

---
+++ Matrix_rotation.java
@@ -0,0 +1,44 @@
+package Rotate;
+
+import java.util.Scanner;
+
+public class Matrix_rotation {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int i,j;
+		int n = sc.nextInt();
+		int arr[][] = new int[n][n];
+		{
+		for( i=0; i<n; i++) {
+			for( j=0;j<n;j++);{
+				arr[i][j] = sc.nextInt();
+			}
+		}
+		rotateImage(arr);
+		for ( i=0;i<n;i++) {
+			for(j=i;j<n;j++) {
+				System.out.println(arr[i][j]+" ");
+			}
+		}
+
+		}
+	}
+	private static void rotateImage(int[][] arr) {
+		int n=arr.length;
+		for(int i=0;i<n;i++) {
+			for(int j=i;j<n;j++) {
+				int a=arr[i][j];
+				arr[i][j]=arr[j][i];
+				arr[j][i]=a;
+	}
+}
+		for(int i=0;i<n;i++) {
+			for(int j=i;j<n/2;j++) {
+				int a=arr[i][j];
+				arr[i][j]=arr[i][n-1-j];
+				arr[i][n-1-j]=a;
+			}
+}
+	}
+}
